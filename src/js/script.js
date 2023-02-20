3e81406a6e012625b6033f66f06e5f46cb0f863f
Ivan <ivanitit3535@gmail.com>
2023-02-20 11:03:27 +0200

    add catalog8-37

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,16 @@
 $(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {                
-                dots: true,
-                arrows: false
-              }
-            },            
-          ]        
-    });
+  $('.carousel__inner').slick({
+    speed: 1200,    
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      },
+    ]
+  });
 });
