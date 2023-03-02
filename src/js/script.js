bca4bc817e50ab55da731409d983392d4ff0113a
Ivan <ivanitit3535@gmail.com>
2023-03-02 15:15:53 +0200

    modal end forms

--- src/js/script.js
+++ src/js/script.js
@@ -45,10 +45,45 @@ toggleSlide('.catalog-item__back');
       $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });    
 
-    $('.button_mini').each(function(i) {
+    $('.button_mini').each(function(I) {
       $(this).on('click', function() {
-          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('#order .modal__deskr').text($('.catalog-item__subtitle').eq(I).text());
           $('.overlay, #order').fadeIn('slow');
-      });
+      })
   });
+
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+38 (999) 999-99-99");
+
+  
 });
