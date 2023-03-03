b909a6e39419725f67cebcbf7ef28c69cbd3ed5f
Ivan <ivanitit3535@gmail.com>
2023-03-03 19:15:24 +0200

    post

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.carousel__inner').slick({
     speed: 1200,
     autoplay: true,
-    autoplaySpeed: 2000,    
+    autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
     responsive: [
@@ -16,74 +16,96 @@ $(document).ready(function () {
     ]
   });
 
-  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
   function toggleSlide(item) {
-    $(item).each(function(i) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    });
-};
-
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
-
-//modal
-
-    $('[data-model=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn('slow');
-    });
-
-    $('.modal__close').on('click', function() {
-      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
-    });    
-
-    $('.button_mini').each(function(I) {
-      $(this).on('click', function() {
-          $('#order .modal__deskr').text($('.catalog-item__subtitle').eq(I).text());
-          $('.overlay, #order').fadeIn('slow');
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
       })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  //modal
+
+  $('[data-model=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
   });
 
-  function validateForms(form){
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function (I) {
+    $(this).on('click', function () {
+      $('#order .modal__deskr').text($('.catalog-item__subtitle').eq(I).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  function validateForms(form) {
     $(form).validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            phone: "required",
-            email: {
-                required: true,
-                email: true
-            }
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
         },
-        messages: {
-            name: {
-                required: "Пожалуйста, введите свое имя",
-                minlength: jQuery.validator.format("Введите {0} символа!")
-              },
-            phone: "Пожалуйста, введите свой номер телефона",
-            email: {
-              required: "Пожалуйста, введите свою почту",
-              email: "Неправильно введен адрес почты"
-            }
+        phone: "required",
+        email: {
+          required: true,
+          email: true
         }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
     });
-};
+  };
 
-validateForms('#consultation-form');
-validateForms('#consultation form');
-validateForms('#order form');
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 
-$('input[name=phone]').mask("+38 (999) 999-99-99");
+  $('input[name=phone]').mask("+38 (999) 999-99-99");
 
-  
+  $('form').submit(function (e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function () {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+
 });
